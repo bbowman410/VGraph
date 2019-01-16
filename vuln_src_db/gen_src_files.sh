d9e3c86ecef195105b2c7c3c80c0e14acaf2ef06
Ben <bbowman410@gmail.com>
2019-01-16 14:34:49 -0500

    fixed bug with logging

--- vuln_src_db/gen_src_files.sh
+++ vuln_src_db/gen_src_files.sh
@@ -17,22 +17,22 @@
 
 SKIPPED=0
 PROCESSED=0
-OUTPUT_FILE='gen_src_files.output'
+OUTPUT_FILE=`pwd`'/gen_src_files.output'
 mkdir -p src_files
 for commit_file in `ls commits`; do
     echo "Generating src files for commit file: $commit_file"
-    echo "Generating src files for commit file: $commit_file" &> $OUTPUT_FILE
+    echo "Generating src files for commit file: $commit_file" >> $OUTPUT_FILE
     commits=`cat commits/$commit_file`
     codebase=`echo $commit_file | awk -F'.' '{print $1}'`
     cd repos/$codebase/
     for c in $commits; do
-        echo "Processing commit $c" &> $OUTPUT_FILE
+        echo "Processing commit $c" >> $OUTPUT_FILE
         let PROCESSED=$PROCESSED+1
         git show $c > ../../scratchwork
         cve=`cat ../../scratchwork | grep -o "CVE-[0-9]*-[0-9]*" | head -1`
         num_files_covered=`grep "^diff --git" ../../scratchwork | wc -l`
         if [ $num_files_covered -gt 5 ] || [ $num_files_covered -eq 0 ]; then
-            echo "This commit covers $num_files_covered files...skipping it." &> $OUTPUT_FILE
+            echo "This commit covers $num_files_covered files...skipping it." >> $OUTPUT_FILE
             let SKIPPED=$SKIPPED+1
             continue
         fi
@@ -53,14 +53,14 @@ for commit_file in `ls commits`; do
             # Repeat 3 for each modification in file
             # Repeat 1,2 for each file in commit
             if [ "`echo $line | grep "^diff --git"`" != "" ]; then
-                echo "Found diff git line: $line" &> $OUTPUT_FILE
+                echo "Found diff git line: $line" >> $OUTPUT_FILE
                 if [ "$curr_file" != "" ] && [ "$curr_funcs" != "" ]; then
                     # signals the end of previous file
                     # we need to write results
-                    echo "Writing results for $curr_file" &> $OUTPUT_FILE
-                    echo "Writing results for $curr_funcs" &> $OUTPUT_FILE
-                    echo "Writing vuln id: $curr_vuln_id" &> $OUTPUT_FILE
-                    echo "Writing patch id: $curr_patch_id" &> $OUTPUT_FILE
+                    echo "Writing results for $curr_file" >> $OUTPUT_FILE
+                    echo "Writing results for $curr_funcs" >> $OUTPUT_FILE
+                    echo "Writing vuln id: $curr_vuln_id" >> $OUTPUT_FILE
+                    echo "Writing patch id: $curr_patch_id" >> $OUTPUT_FILE
                     mkdir -p ../../src_files/$codebase/$cve/vuln
                     mkdir -p ../../src_files/$codebase/$cve/patch
                     git show $curr_vuln_id > ../../src_files/$codebase/$cve/vuln/$curr_file
@@ -73,18 +73,18 @@ for commit_file in `ls commits`; do
                 curr_vuln_id=""
                 curr_patch_id=""
                 if [ "$curr_file" != "" ]; then
-                    echo "Started processing file: $curr_file" &> $OUTPUT_FILE
+                    echo "Started processing file: $curr_file" >> $OUTPUT_FILE
                 fi
             elif [ "$curr_file" != "" ] && [ "`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*"`" != "" ]; then
-                echo "Found index line: $line" &> $OUTPUT_FILE
+                echo "Found index line: $line" >> $OUTPUT_FILE
                 curr_vuln_id=`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*" | awk '{print $2}' | tr '..' ' ' | awk '{print $1}'`
                 curr_patch_id=`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*" | awk '{print $2}' | tr '..' ' ' | awk '{print $2}'`
             elif [ "$curr_file" != "" ] && [ "$curr_vuln_id" != "" ] && [ "$curr_patch_id" != "" ] && [ "`echo $line | grep "@@" | grep -o "[a-zA-Z0-9_]*(" | sed "s/(//g" | uniq`" != "" ]; then
                 # At this point we have everything we need to write results...so lets do that
                 func_name="`echo $line | grep "@@" | grep -o "[a-zA-Z0-9_]*(" | sed "s/(//g" | tail -1`" # only get the last occurence in case there are some weird return types
                 curr_funcs="${curr_funcs}${func_name} " # keep track of all
-                echo "Found function names: $func_name" &> $OUTPUT_FILE
-                echo "Writing results" &> $OUTPUT_FILE
+                echo "Found function names: $func_name" >> $OUTPUT_FILE
+                echo "Writing results" >> $OUTPUT_FILE
 
             else
                 # echo "Skipping line: $line"
@@ -93,12 +93,12 @@ for commit_file in `ls commits`; do
         done < <(cat ../../scratchwork)
 
         if [ "$curr_file" != "" ] && [ "$curr_funcs" != "" ] && [ "$curr_vuln_id" != "" ] && [ "$curr_patch_id" != "" ]; then
-            echo "Writing results for $curr_file" &> $OUTPUT_FILE
-            echo "$cve" &> $OUTPUT_FILE
-            echo "$codebase" &> $OUTPUT_FILE
-            echo "Writing results for $curr_funcs" &> $OUTPUT_FILE
-            echo "Writing vuln id: $curr_vuln_id" &> $OUTPUT_FILE
-            echo "Writing patch id: $curr_patch_id" &> $OUTPUT_FILE
+            echo "Writing results for $curr_file" >> $OUTPUT_FILE
+            echo "$cve" >> $OUTPUT_FILE
+            echo "$codebase" >> $OUTPUT_FILE
+            echo "Writing results for $curr_funcs" >> $OUTPUT_FILE
+            echo "Writing vuln id: $curr_vuln_id" >> $OUTPUT_FILE
+            echo "Writing patch id: $curr_patch_id" >> $OUTPUT_FILE
             mkdir -p ../../src_files/$codebase/$cve/vuln
             mkdir -p ../../src_files/$codebase/$cve/patch
             git show $curr_vuln_id > ../../src_files/$codebase/$cve/vuln/$curr_file
@@ -109,5 +109,5 @@ for commit_file in `ls commits`; do
     cd ../../
 done
 rm -f scratchwork
-echo "PROCESSED: $PROCESSED SKIPPED: $SKIPPED" &> $OUTPUT_FILE
+echo "PROCESSED: $PROCESSED SKIPPED: $SKIPPED" >> $OUTPUT_FILE
 echo "PROCESSED: $PROCESSED SKIPPED: $SKIPPED" 
