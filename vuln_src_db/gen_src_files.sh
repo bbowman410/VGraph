27c0588b04f54e0a5823f148e7dd0e3ed543bb95
Ben <bbowman410@gmail.com>
2019-03-05 12:50:56 -0500

    Made this script suck less.  Also started pulling revisions before and after vulnerability patch

--- vuln_src_db/gen_src_files.sh
+++ vuln_src_db/gen_src_files.sh
@@ -15,99 +15,153 @@
 # we know which functions are modified from vuln -> patch.  This helps with
 # processing later.
 
+LOG_FILE=`pwd`'/gen_src_files.output'
+SCRATCH_FILE=`pwd`'/scratch'
+SRC_CODE_DIR=`pwd`'/src_files'
 SKIPPED=0
 PROCESSED=0
-OUTPUT_FILE=`pwd`'/gen_src_files.output'
-mkdir -p src_files
-for commit_file in `ls commits`; do
-    echo "Generating src files for commit file: $commit_file"
-    echo "Generating src files for commit file: $commit_file" >> $OUTPUT_FILE
-    commits=`cat commits/$commit_file`
-    codebase=`echo $commit_file | awk -F'.' '{print $1}'`
-    cd repos/$codebase/
-    for c in $commits; do
-        echo "Processing commit $c" >> $OUTPUT_FILE
-        let PROCESSED=$PROCESSED+1
-        git show $c > ../../scratchwork
-        cve=`cat ../../scratchwork | grep -o "CVE-[0-9]*-[0-9]*" | head -1`
-        num_files_covered=`grep "^diff --git" ../../scratchwork | wc -l`
-        if [ $num_files_covered -gt 5 ] || [ $num_files_covered -eq 0 ]; then
-            echo "This commit covers $num_files_covered files...skipping it." >> $OUTPUT_FILE
-            let SKIPPED=$SKIPPED+1
-            continue
-        fi
 
-        curr_file=""
-        curr_funcs=""
-        curr_vuln_id=""
-        curr_patch_id=""
+function download_data {
+  # Read arguments
+  local commit_hash=$1
+  local codebase=$2
+  local cve=$3
+  local path=$4
+  local funcs=$5
+  local vuln_hash=$6
+  local patch_hash=$7
+  
+  log "Downloading source code for: $cve $codebase $path $funcs $commit_hash"
 
-        while read line; do
+  # Make any necessary directories
+  mkdir -p $SRC_CODE_DIR/$codebase/$cve/vuln
+  mkdir -p $SRC_CODE_DIR/$codebase/$cve/patch
+  mkdir -p $SRC_CODE_DIR/$codebase/$cve/before
+  mkdir -p $SRC_CODE_DIR/$codebase/$cve/after
 
-            # consume each line of the Github commit.
-            # commits will follow this structure:
-            # (1) diff --git a/path/file b/path/file
-            # (2) index <hash>..<hash> number
-            # (3) @@ location $$ function
-
-            # Repeat 3 for each modification in file
-            # Repeat 1,2 for each file in commit
-            if [ "`echo $line | grep "^diff --git"`" != "" ]; then
-                echo "Found diff git line: $line" >> $OUTPUT_FILE
-                if [ "$curr_file" != "" ] && [ "$curr_funcs" != "" ]; then
-                    # signals the end of previous file
-                    # we need to write results
-                    echo "Writing results for $curr_file" >> $OUTPUT_FILE
-                    echo "Writing results for $curr_funcs" >> $OUTPUT_FILE
-                    echo "Writing vuln id: $curr_vuln_id" >> $OUTPUT_FILE
-                    echo "Writing patch id: $curr_patch_id" >> $OUTPUT_FILE
-                    mkdir -p ../../src_files/$codebase/$cve/vuln
-                    mkdir -p ../../src_files/$codebase/$cve/patch
-                    git show $curr_vuln_id > ../../src_files/$codebase/$cve/vuln/$curr_file
-                    git show $curr_patch_id > ../../src_files/$codebase/$cve/patch/$curr_file
-                    echo "$curr_funcs" | tr ' ' '\n' | uniq > ../../src_files/$codebase/$cve/funcnames
-                fi
-                # Set new filename.  If its not a C/C++ file then curr_file will be blank and no processing will occur
-                curr_file=`echo $line | grep -o -m 1 "[a-zA-Z0-9_]*\.c[pp]*" | uniq`
-                curr_funcs=""
-                curr_vuln_id=""
-                curr_patch_id=""
-                if [ "$curr_file" != "" ]; then
-                    echo "Started processing file: $curr_file" >> $OUTPUT_FILE
-                fi
-            elif [ "$curr_file" != "" ] && [ "`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*"`" != "" ]; then
-                echo "Found index line: $line" >> $OUTPUT_FILE
-                curr_vuln_id=`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*" | awk '{print $2}' | tr '..' ' ' | awk '{print $1}'`
-                curr_patch_id=`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*" | awk '{print $2}' | tr '..' ' ' | awk '{print $2}'`
-            elif [ "$curr_file" != "" ] && [ "$curr_vuln_id" != "" ] && [ "$curr_patch_id" != "" ] && [ "`echo $line | grep "@@" | grep -o "[a-zA-Z0-9_]*(" | sed "s/(//g" | uniq`" != "" ]; then
-                # At this point we have everything we need to write results...so lets do that
-                func_name="`echo $line | grep "@@" | grep -o "[a-zA-Z0-9_]*(" | sed "s/(//g" | tail -1`" # only get the last occurence in case there are some weird return types
-                curr_funcs="${curr_funcs}${func_name} " # keep track of all
-                echo "Found function names: $func_name" >> $OUTPUT_FILE
-                echo "Writing results" >> $OUTPUT_FILE
-
-            else
-                # echo "Skipping line: $line"
-                continue
-            fi
-        done < <(cat ../../scratchwork)
-
-        if [ "$curr_file" != "" ] && [ "$curr_funcs" != "" ] && [ "$curr_vuln_id" != "" ] && [ "$curr_patch_id" != "" ]; then
-            echo "Writing results for $curr_file" >> $OUTPUT_FILE
-            echo "$cve" >> $OUTPUT_FILE
-            echo "$codebase" >> $OUTPUT_FILE
-            echo "Writing results for $curr_funcs" >> $OUTPUT_FILE
-            echo "Writing vuln id: $curr_vuln_id" >> $OUTPUT_FILE
-            echo "Writing patch id: $curr_patch_id" >> $OUTPUT_FILE
-            mkdir -p ../../src_files/$codebase/$cve/vuln
-            mkdir -p ../../src_files/$codebase/$cve/patch
-            git show $curr_vuln_id > ../../src_files/$codebase/$cve/vuln/$curr_file
-            git show $curr_patch_id > ../../src_files/$codebase/$cve/patch/$curr_file
-            echo "$curr_funcs" | tr ' ' '\n' | uniq > ../../src_files/$codebase/$cve/funcnames
-        fi
+  # Download data from github.  save function names
+  git show $curr_vuln_id > $SRC_CODE_DIR/$codebase/$cve/vuln/$curr_file
+  git show $curr_patch_id > $SRC_CODE_DIR/$codebase/$cve/patch/$curr_file
+  echo "$funcs" | tr ' ' '\n' | uniq > $SRC_CODE_DIR/$codebase/$cve/funcnames
+  
+  # Identify 2 commits before this one.  Write files.
+  before_commits=`git log $path | grep "^commit" | grep -B 2 $commit_hash | head -2`
+  if [ "$before_commits" != "" ]; then
+    for bc in $before_commits; do
+      if [ "$bc" != "commit" ]; then
+        log "Downloading (addtl before vuln) source code for: $cve $codebase $path $funcs $bc"
+        git show $bc:$path > $SRC_CODE_DIR/$codebase/$cve/before/${bc}_${curr_file}
+      fi
     done
-    cd ../../
-done
-rm -f scratchwork
-echo "PROCESSED: $PROCESSED SKIPPED: $SKIPPED" >> $OUTPUT_FILE
-echo "PROCESSED: $PROCESSED SKIPPED: $SKIPPED" 
+  fi
+
+  # Identify 2 commits after this one.  Write files.
+  after_commits=`git log $path | grep "^commit" | grep -A 2 $commit_hash | tail -2`
+  if [ "$after_commits" != "" ]; then
+    for ac in $after_commits; do
+      if [ "$ac" != "commit" ]; then
+        log "Downloading (addtl after patch) source code for: $cve $codebase $path $funcs $ac"
+        git show $ac:$path > $SRC_CODE_DIR/$codebase/$cve/after/${ac}_${curr_file}
+      fi
+    done
+  fi
+}
+
+# This function is called after the git show command is called
+# and the output is written to SCRATCH_FILE
+# This function is responsible for actually parsing the file lines
+# and downloading the relevant source code
+function process_commit_lines {
+  local commit_hash=$1
+  local codebase=$2
+  local cve=$3
+
+  local curr_file=""
+  local curr_funcs=""
+  local curr_vuln_id=""
+  local curr_patch_id=""
+  local path=""
+
+  while read line; do
+    if [ "`echo $line | grep "^diff --git"`" != "" ]; then
+      log "Found diff line: $line"
+      if [ "$curr_file" != "" ] && [ "$curr_funcs" != "" ]; then
+        download_data $commit_hash $codebase $cve $path $curr_funcs $curr_vuln_id $curr_patch_id
+      fi
+      # Set new filename.  If its not C/C++ file then curr_filew ill be blank
+      local curr_file=`echo $line | grep -o -m 1 "[a-zA-Z0-9_]*\.c[pp]*" | uniq`
+      local path=`echo $line | grep -o -m 1 "a\/[\/a-zA-Z0-9_-]*\.c[pp]*" | uniq`
+      local path=${path:2} # remove the 'a/' from front
+      local curr_funcs=""
+      local curr_vuln_id=""
+      local curr_patch_id=""
+      if [ "$curr_file" != "" ]; then
+        log "Parsing modification to file: $curr_file"
+      fi
+    elif [ "$curr_file" != "" ] && [ "`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*"`" != "" ]; then
+      log "Found index line: $line"
+      local curr_vuln_id=`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*" | awk '{print $2}' | tr '..' ' ' | awk '{print $1}'`
+      local curr_patch_id=`echo $line | grep -o "^index [a-z0-9]*\.\.[a-z0-9]*" | awk '{print $2}' | tr '..' ' ' | awk '{print $2}'`
+    elif [ "$curr_file" != "" ] && [ "$curr_vuln_id" != "" ] && [ "$curr_patch_id" != "" ] && [ "`echo $line | grep "@@" | grep -o "[a-zA-Z0-9_]*(" | sed "s/(//g" | uniq`" != "" ]; then
+      log "Found function line: $line"
+      local func_name="`echo $line | grep "@@" | grep -o "[a-zA-Z0-9_]*(" | sed "s/(//g" | tail -1`" # only get the last occurence in case there are some weird return types
+      local curr_funcs="${curr_funcs}${func_name} " # keep track of all
+    else
+      # Nothing important on this line.. skip it
+      continue
+    fi
+  done < <(cat $SCRATCH_FILE)
+  
+  # We still may need to write out a file
+  if [ "$curr_file" != "" ] && [ "$curr_funcs" != "" ] && [ "$curr_vuln_id" != "" ] && [ "$curr_patch_id" != "" ]; then
+    download_data $commit_hash $codebase $cve $path $curr_funcs $curr_vuln_id $curr_patch_id
+  fi
+}
+
+function process_commit {
+  local commit=$1
+  local codebase=$2
+  log "Processing commit: $codebase $commit"
+  git show $commit > $SCRATCH_FILE 
+  local cve=`cat $SCRATCH_FILE | grep -o "CVE-[0-9]*-[0-9]*" | head -1`
+  local num_files_covered=`grep "^diff --git" $SCRATCH_FILE | wc -l`
+  if [ $num_files_covered -gt 5 ] || [ $num_files_covered -eq 0 ]; then
+    log "ERROR: This commit covers $num_files_covered files. Skipping it."
+    let SKIPPED=$SKIPPED+1
+  else
+    process_commit_lines $commit $codebase $cve
+  fi
+  rm $SCRATCH_FILE
+}
+
+function process_commit_file {
+  local commit_file=$1
+  local commits=`cat commits/$commit_file`
+  local codebase=`echo $commit_file | awk -F'.' '{print $1}'`
+  
+  log "Processing commit file: $commit_file"
+ 
+  cd repos/$codebase # We have to change directory to use git commands
+  for c in $commits; do
+    process_commit $c $codebase
+  done
+  cd ../../
+}
+
+function log {
+  echo "$1" >> $LOG_FILE
+}
+
+function main {
+  mkdir -p src_files
+  cat /dev/null > $LOG_FILE # Reset log  
+
+  echo "Logging to $LOG_FILE..."
+
+  for commit_file in `ls commits`; do
+    process_commit_file $commit_file
+  done
+  log "Finished.  Processed $PROCESSED, Skipped $SKIPPED"
+}
+
+main
